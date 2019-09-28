b354e66b7fea2af05125d63afffeba709ed33b39
Yitbarek Gitore <yitbareky@gmail.com>
2019-09-28 14:02:38 -0500

    complete

--- javascripts/components/condiments.js
+++ javascripts/components/condiments.js
@@ -1,5 +1,5 @@
 import utilities from '../helpers/utilities.js';
-const condiment = [{id: "condiment1", name: "Chipotle Mayonnaise", price: 50},
+const condiment = [{id: "condiment1", name: "Mayonnaise", price: 50},
 {id: "condiment2", name: "Wasabi Mayonnaise", price: 12},
 {id: "condiment3", name: "Hoisin Sauce", price: 67},
 {id: "condiment4", name: "Tonkatsu Sauce", price: 35}
@@ -10,12 +10,37 @@ const printCondimentsOptions = ()=>{
     for (let i = 0; i < condiment.length; i++) {
         domString += `
                 <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input cheese" id="${condiment[i].id}">
+                <input type="checkbox" class="form-check-input condiments" id="${condiment[i].id}">
                 <label class="form-check-label" for="${condiment[i].id}">${condiment[i].name}</label>
             </div>`;
         
     }    
     utilities.printToDom('condiments-container',domString);
 };
+const getSelectedCondiments = ()=>{
+    const selectedCondiments = [];
+    const allCondiments = document.getElementsByClassName('condiments');
+    for (let i = 0; i < allCondiments.length; i++) {
+        for (let j = 0; j < condiment.length; j++) {
+            if(allCondiments[i].checked && allCondiments[i].id === condiment[j].id){
+                selectedCondiments.push(condiment[j]);
+                allCondiments[i].checked = false;
+            }
+        }
+    }
+    return selectedCondiments;
+}
 
+const addCondimentsToDom = ()=>{
+    const condimentsSelected = getSelectedCondiments();
+    let condimentsString = '<div id = "bread-String">';
+    for (let i = 0; i < condimentsSelected.length; i++) {
+        condimentsString += `<p>Condiments:${condimentsSelected[i].name}</p> 
+        <p>Price: ${condimentsSelected[i].price}</p>`;        
+    }
+    condimentsString +='</div>';
+    utilities.printToDom('my-condiments-selection', condimentsString);
+
+}
+document.body.addEventListener('keyup',addCondimentsToDom);
 export default{ printCondimentsOptions };
